51ef0f2dc23a3eafca67d9a75be40c262f6522f7
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-03-18 17:54:56 +0000

    Replaces JavaScript with jQuery to enable smooth show and hide effects on projects

--- app.js
+++ app.js
@@ -1,22 +1,89 @@
 console.log('loaded')
 
+$(function() {
 
 
-function onClick(e) {
-  // console.log(e.classList)
-  const content = e.previousElementSibling
-  // console.log(content)
-  if (content.style.display === 'flex') {
-    content.style.display = 'none'
-    e.classList.remove('hide-button')
-    e.classList.add('show-button')
-  } else {
-    content.style.display = 'flex'
-    e.classList.remove('show-button')
-    e.classList.add('hide-button')
-  }
-}
+  // $('.show-button').click(function() {
+  //
+  // })
 
+  $('.project-content').hide()
+
+  $('button').click(function () {
+    $(this).prev('.project-content').slideToggle()
+    $(this).prev('.project-content').css('display', 'flex')
+    $(this).toggleClass('show-button hide-button')
+  })
+
+})
+
+
+
+
+
+// function onClick(e) {
+//   const content = e.previousElementSibling
+//   if (content.style.display === 'flex') {
+//     content.style.display = 'none'
+//     e.classList.remove('hide-button')
+//     e.classList.add('show-button')
+//   } else {
+//     content.style.display = 'flex'
+//     e.classList.remove('show-button')
+//     e.classList.add('hide-button')
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function onClick(e) {
+//   // console.log(e.classList)
+//   const content = e.previousElementSibling
+//   content.classList.toggle('hide')
+//   // console.log(content)
+//   if (content.classList.contains === 'title-area') {
+//     // content.classList.remove('show')
+//     // content.classList.add('hide')
+//     e.classList.remove('hide-button')
+//     e.classList.add('show-button')
+//   } else {
+//     // content.classList.remove('hide')
+//     // content.classList.add('show')
+//
+//     // content.style.display = 'flex'
+//     e.classList.remove('show-button')
+//     e.classList.add('hide-button')
+//   }
+// }
 
 
 
