473e443d683bf40196074fac8fc2a43a3f969abd
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-03-18 15:12:11 +0000

    Refactored JS so click events are handled in one function

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 console.log('loaded')
 
-function showSpace() {
-  const space = document.querySelector('.space')
-  if (space.style.display === 'flex') space.style.display = 'none'
-  else space.style.display = 'flex'
+
+
+function onClick(e) {
+  // console.log(e.classList)
+  const content = e.previousElementSibling
+  // console.log(content)
+  if (content.style.display === 'flex') {
+    content.style.display = 'none'
+    e.classList.remove('hide-button')
+    e.classList.add('show-button')
+  } else {
+    content.style.display = 'flex'
+    e.classList.remove('show-button')
+    e.classList.add('hide-button')
+  }
 }
 
-function showHackathon() {
-  const space = document.querySelector('.hackathon')
-  if (space.style.display === 'flex') space.style.display = 'none'
-  else space.style.display = 'flex'
-}
 
-function showTravel() {
-  const space = document.querySelector('.travel')
-  if (space.style.display === 'flex') space.style.display = 'none'
-  else space.style.display = 'flex'
-}
 
-function showBarlevard() {
-  const space = document.querySelector('.barlevard')
-  if (space.style.display === 'flex') space.style.display = 'none'
-  else space.style.display = 'flex'
-}
+
+// function showSpace() {
+//   const space = document.querySelector('.space')
+//   const button = document.querySelector('.space-button')
+//   if (space.style.display === 'flex') {
+//     space.style.display = 'none'
+//     button.classList.remove('hide-button')
+//     button.classList.add('show-button')
+//   } else {
+//     space.style.display = 'flex'
+//     button.classList.remove('show-button')
+//     button.classList.add('hide-button')
+//   }
+// }
+
+// function showHackathon() {
+//   const space = document.querySelector('.hackathon')
+//   if (space.style.display === 'flex') space.style.display = 'none'
+//   else space.style.display = 'flex'
+// }
+//
+// function showTravel() {
+//   const space = document.querySelector('.travel')
+//   if (space.style.display === 'flex') space.style.display = 'none'
+//   else space.style.display = 'flex'
+// }
+//
+// function showBarlevard() {
+//   const space = document.querySelector('.barlevard')
+//   if (space.style.display === 'flex') space.style.display = 'none'
+//   else space.style.display = 'flex'
+// }
