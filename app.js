3177e7c443a21fb8d5128a759317e64b5c4e0156
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-03-18 10:36:01 +0000

    Have a show and hide function for projects on main page

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log('loaded')
+
+function showSpace() {
+  const space = document.querySelector('.space')
+  if (space.style.display === 'flex') space.style.display = 'none'
+  else space.style.display = 'flex'
+}
+
+function showHackathon() {
+  const space = document.querySelector('.hackathon')
+  if (space.style.display === 'flex') space.style.display = 'none'
+  else space.style.display = 'flex'
+}
+
+function showTravel() {
+  const space = document.querySelector('.travel')
+  if (space.style.display === 'flex') space.style.display = 'none'
+  else space.style.display = 'flex'
+}
+
+function showBarlevard() {
+  const space = document.querySelector('.barlevard')
+  if (space.style.display === 'flex') space.style.display = 'none'
+  else space.style.display = 'flex'
+}
