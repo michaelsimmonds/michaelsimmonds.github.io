7138f1e29eabc312ee51d1d1de15b6ea91eedbb1
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-04-02 15:40:14 +0100

    Added faded in on load

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ console.log('loaded')
 
 $('DOM content loaded', function() {
 
+  $('#hero').animate({ opacity: 1 }, { duration: 3000 })
+
   $('.project-content').hide()
 
   $('button').click(function () {
